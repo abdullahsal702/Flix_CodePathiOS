baefb1ac55c9a9aabc75dd3e9ee8374e391906b4
abdullahsal702 <88620119+abdullahsal702@users.noreply.github.com>
2021-09-25 13:57:47 -0400

    part 2 done

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Abdullah Saleh on 9/24/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
